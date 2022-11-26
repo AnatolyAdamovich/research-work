d7da56a87513a02eeb1871b376e69ce83ff8e548
ffrankusha <tolik.frnk@yandex.ru>
2022-11-27 00:17:37 +0300

    experiments with new optimizer & code refactoring

--- train_test_help_functions.py
+++ train_test_help_functions.py
@@ -6,7 +6,7 @@ def training_model(model, optimizer_fn, loss_fn, metric_fn,
                    learning_rate=1e-3, current_device="cpu",
                    epochs=None, max_epochs=1e4, min_score=0.95,
                    valid_period=5, printed=False,
-                   with_addition=False):
+                   with_addition=False, new_optim=False):
     """Train model
     Parameters:
         model (torch.nn.Module object): neural network model
@@ -23,6 +23,7 @@ def training_model(model, optimizer_fn, loss_fn, metric_fn,
         valid_period (int): how often we evaluate our model (each 'valid_period' epoch)
         printed (bool): print score and loss in validation phase or not
         with_addition (bool): do we use loss function with adj(X) or not (see 'new_loss.png' file)
+        new_optim (bool): do we use new optimizer or not
     """
     model = model.to(current_device)
     if type(optimizer_fn) == type:
@@ -30,33 +31,47 @@ def training_model(model, optimizer_fn, loss_fn, metric_fn,
                                  lr=learning_rate)
     else:
         optimizer = optimizer_fn
+
+    loss_train_array = []
+    loss_test_array = []
+    score_test_array = []
+
     if epochs:
         for epoch in range(1, epochs+1):
-            train_epoch(model, optimizer, loss_fn, metric_fn, data_train, current_device, with_addition)
+            loss_epoch = train_epoch(model, optimizer, loss_fn, metric_fn, data_train, current_device, with_addition, new_optim)
+            loss_train_array.append(loss_epoch)
+
             # evaluate
             if epoch % valid_period == 0:
                 loss, metric = validation_epoch(model, loss_fn, metric_fn, data_test, current_device)
+                loss_test_array.append(loss)
+                score_test_array.append(metric)
+
                 if printed:
                     print(f'epoch {epoch}: loss = {loss:.3f} and score = {metric:.3f}')
+
     else:
         metric = 0.0
         epoch = 0
         while (metric < min_score) and (epoch < max_epochs):
             # train
-            train_epoch(model, optimizer, loss_fn, metric_fn, data_train, current_device, with_addition)
+            loss_epoch = train_epoch(model, optimizer, loss_fn, metric_fn, data_train, current_device, with_addition, new_optim)
+            loss_train_array.append(loss_epoch)
 
             # evaluate
             if epoch % valid_period == 0:
                 loss, metric = validation_epoch(model, loss_fn, metric_fn, data_test, current_device)
+                loss_test_array.append(loss)
+                score_test_array.append(metric)
                 if printed:
-                    print(f'epoch {epoch}: loss = {loss:.3f} and score = {metric:.3f}')
+                    print(f'epoch {epoch+1}: loss = {loss:.3f} and score = {metric:.3f}')
             epoch += 1
         return epoch, metric
 
 
-def train_epoch(model, optimizer, loss_fn, metric_fn, data_train, current_device, with_addition=False):
+def train_epoch(model, optimizer, loss_fn, metric_fn, data_train, current_device="cpu", with_addition=False, new_optim=False):
     """One epoch in train cycle"""
-
+    mean_loss_epoch = 0.0
     model.train()
     for X_batch, y_batch in data_train:
         X_batch, y_batch = X_batch.to(current_device), y_batch.to(current_device)
@@ -71,9 +86,11 @@ def train_epoch(model, optimizer, loss_fn, metric_fn, data_train, current_device
             inverse_batch = torch.linalg.inv(X_batch)
             adjoint = determinant * inverse_batch
             loss = loss_fn(adjoint @ predicted, adjoint @ y_batch)
+
         else:
             loss = loss_fn(predicted, y_batch)
 
+        mean_loss_epoch += loss
         # zero gradient
         optimizer.zero_grad()
 
@@ -81,7 +98,13 @@ def train_epoch(model, optimizer, loss_fn, metric_fn, data_train, current_device
         loss.backward()
 
         # update model parameters
-        optimizer.step()
+        if new_optim:
+            optimizer.step(determinant_X_batch=determinant)
+        else:
+            optimizer.step()
+
+    mean_loss_epoch /= len(data_train)
+    return mean_loss_epoch
 
 
 def validation_epoch(model, loss_fn, metric_fn, data_test, current_device):
