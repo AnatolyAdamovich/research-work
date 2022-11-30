4252a7580b55d15adc29b9238a08537b63b2a14c
ffrankusha <tolik.frnk@yandex.ru>
2022-12-01 00:14:34 +0300

    create structure for finite-time optimizer

---
+++ optimizers/Finite_Time_Optimizer.py
@@ -0,0 +1,87 @@
+"""
+This script contains an implementation of the optimizer
+that uses a two-phase approach,
+where the DREM operator is used in the 1st phase
+and finite time estimation is used in the 2nd phase
+
+More info can be found in the `report` folder
+"""
+
+import torch
+from torch.optim import Optimizer
+from scipy.interpolate import interp1d
+from scipy.integrate import quad
+
+
+class FiniteTimeOptimizer(Optimizer):
+    def __init__(self, params, lr, n_of_batches):
+
+        if lr < 0.0:
+            raise ValueError(f"Invalid learning rate: {lr} - should be >= 0.0")
+        defaults = {"lr": lr, "number_of_batches": n_of_batches}
+
+        super(FiniteTimeOptimizer, self).__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self, det_batch, partition=0, t=0, closure=None):
+        """
+        single optimization step
+        Parameters:
+            det_batch (torch.tensor or float):
+            partition (int): batch's part
+            t (int): batch's number (from 1 to self.state['number_of_batches'])
+            closure (callable): functionality to recalculate loss function // see torch.optim docs for more
+        """
+
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            lr = group["lr"]
+            n_of_batches = group['number_of_batches']
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                gradient = p.grad.data
+                state = self.state[p]
+                if t == n_of_batches:
+                    self._finite_time(state, n_of_batches, lr)
+                else:
+                    self._drem_operator(det_batch, partition, state, lr, gradient, p)
+
+        return loss
+
+    def _drem_operator(self, det_batch, partition, state, lr, gradient, p):
+        """Functional API that performs optimizers computations"""
+        state['determinants'] = state.get("determinants", []).append(det_batch)
+        if partition == 0:
+            # case 1: batch size == number of input feature (in the 1st layer)
+            state["step"] = state.get("step", 0) + 1
+            gradient.div_(1 + lr * (det_batch ** 2))
+            p.data.add_(gradient, alpha=-lr)
+        else:
+            # case 2: batch size == s * n, where n == number of input feature (in the 1st layer)
+            state['part'] = state.get("part", 0) + 1
+            gradient.div_(1 + lr * (det_batch ** 2))
+            state["cum_grad"] = state.get("cum_grad", 0.0) + (lr * gradient)
+            if partition == 1:
+                mean_cum_grad = state["cum_grad"] / state["part"]
+                p.data.add_(mean_cum_grad, alpha=-1)
+
+    def _finite_time(self, state, n_of_batches, lr):
+        x = torch.arange(len(state['determinants']))
+        determinant_function = interp1d(x, state['determinants'],
+                                        fill_value='extrapolate')
+
+        def square_det(a):
+            return determinant_function(a)**2
+
+        w = quad(func=square_det,
+                 a=0,
+                 b=n_of_batches)
+
+        w = torch.exp(-lr * w)
+
+
